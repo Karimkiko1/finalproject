c8c68e65b72da8c61c30ad8a77decedd2c3ed9bc
Karim-Said1 <karimsaidsalama1@gmail.com>
2025-05-15 01:19:00 +0000

    new file:   frontend/dist/bundle.js 	new file:   frontend/dist/vendors-frontend_node_modules_canvg_lib_index_es_js.bundle.js 	new file:   frontend/dist/vendors-frontend_node_modules_dompurify_dist_purify_es_mjs.bundle.js 	new file:   frontend/dist/vendors-frontend_node_modules_html2canvas_dist_html2canvas_js.bundle.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,16 @@ module.exports = {
         test: /\.(js|jsx)$/, // مهم إنه يشمل .jsx
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
     ]
   },
   resolve: {
