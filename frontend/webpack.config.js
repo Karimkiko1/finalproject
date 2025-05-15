52b00e2a1992af6d719629dc79b950ea1c834603
Karimkiko1 <karimkiko929@gmail.com>
2025-05-15 16:11:11 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   frontend/.babelrc
    	deleted:    frontend/dist/229.bundle.js
    	deleted:    frontend/dist/229.bundle.js.LICENSE.txt
    	deleted:    frontend/dist/238.bundle.js
    	deleted:    frontend/dist/801.bundle.js
    	deleted:    frontend/dist/801.bundle.js.LICENSE.txt
    	deleted:    frontend/dist/bundle.25da33361f4956907cad.js
    	deleted:    frontend/dist/bundle.3e690cdc9b92dd7ab2b6.js
    	deleted:    frontend/dist/bundle.3e690cdc9b92dd7ab2b6.js.LICENSE.txt
    	deleted:    frontend/dist/bundle.9e2088c18610a2659df5.js
    	deleted:    frontend/dist/bundle.9e2088c18610a2659df5.js.LICENSE.txt
    	deleted:    frontend/dist/bundle.bce892a90cabcd0a0c44.js
    	deleted:    frontend/dist/bundle.bce892a90cabcd0a0c44.js.LICENSE.txt
    	modified:   frontend/dist/bundle.js
    	deleted:    frontend/dist/bundle.js.LICENSE.txt
    	modified:   frontend/dist/index.html
    	deleted:    frontend/dist/vendors-frontend_node_modules_canvg_lib_index_es_js.bundle.js
    	new file:   frontend/dist/vendors-node_modules_canvg_lib_index_es_js.bundle.js
    	renamed:    frontend/dist/vendors-frontend_node_modules_dompurify_dist_purify_es_mjs.bundle.js -> frontend/dist/vendors-node_modules_dompurify_dist_purify_es_mjs.bundle.js
    	renamed:    frontend/dist/vendors-frontend_node_modules_html2canvas_dist_html2canvas_js.bundle.js -> frontend/dist/vendors-node_modules_html2canvas_dist_html2canvas_js.bundle.js
    	modified:   frontend/node_modules/.package-lock.json
    	modified:   frontend/package-lock.json
    	modified:   frontend/package.json
    	new file:   frontend/public/_redirects
    	modified:   frontend/src/pages/CBMCalculator.jsx
    	deleted:    frontend/vercel.json
    	new file:   frontend/webpack.config.js

---
+++ frontend/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      "fs": false,
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "util": require.resolve("util/"),
+      "assert": require.resolve("assert/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "url": require.resolve("url/"),
+      "vm": require.resolve("vm-browserify"),
+      "constants": require.resolve("constants-browserify"),
+      "zlib": require.resolve("browserify-zlib")
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  mode: 'development',
+};
